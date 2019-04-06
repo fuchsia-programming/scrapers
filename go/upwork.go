9384afc72b8a9a2368e0df6c823469673f76c881
John Bampton <jbampton@users.noreply.github.com>
2019-04-06 11:14:21 +1000

    Add FQL file and Upwork Go based scraper

--- go/upwork.go
+++ go/upwork.go
@@ -1,23 +1,40 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
-	"github.com/levigross/grequests"
+	"github.com/MontFerret/ferret/pkg/compiler"
+	"github.com/MontFerret/ferret/pkg/drivers"
+	"github.com/MontFerret/ferret/pkg/drivers/cdp"
+	"github.com/MontFerret/ferret/pkg/drivers/http"
 	"log"
 )
 
 func main() {
-	//upwork := "www.upwork.com"
-	//upworkurl := "https://www.upwork.com/o/profiles/users/%s"
-	//var record []string
-
-	resp, err := grequests.Get("https://www.upwork.com/fl/johnbampton", nil)
-	// You can modify the request by passing an optional RequestOptions struct
-
+	query := `
+			LET doc = DOCUMENT("https://www.upwork.com/fl/johnbampton", true)
+			WAIT_ELEMENT(doc, 'span[itemprop="pricerange"]')
+			LET price = ELEMENTS(doc, 'span[itemprop="pricerange"]')
+				RETURN {rate: price[1]}
+	`
+	comp := compiler.New()
+	program, err := comp.Compile(query)
 	if err != nil {
-		log.Fatalln("Unable to make request: ", err)
+		log.Println(err)
 	}
-
-	fmt.Println(resp.String())
+	ctx := context.Background()
+	ctx = drivers.WithContext(ctx, cdp.NewDriver())
+	ctx = drivers.WithContext(ctx, http.NewDriver(), drivers.AsDefault())
+	out, err := program.Run(ctx)
+	if err != nil {
+		log.Println(err)
+	}
+	var res map[string]interface{}
+	err = json.Unmarshal(out, &res)
+	if err != nil {
+		log.Println(err)
+	}
+	rate := res["rate"]
+	fmt.Println(rate)
 }
-

74039ac8a55328557839cd7f7dd3ee20564cec4b
John Bampton <jbampton@users.noreply.github.com>
2019-10-17 06:24:54 +1000

    Test shell script on CircleCI. (#16)

--- shell/random-fuchsia-logo.sh
+++ shell/random-fuchsia-logo.sh
@@ -2,4 +2,14 @@
 
 mkdir -p random
 cd random || exit
-curl -O https://raw.githubusercontent.com/fuchsia-programming/fuchsia-programming.github.io/master/assets/images/logos/fuchsia-$(( ( RANDOM % 307 ) )).png
+frandom=$((RANDOM%307))
+curl -O https://raw.githubusercontent.com/fuchsia-programming/fuchsia-programming.github.io/master/assets/images/logos/fuchsia-$((frandom)).png
+
+if [ -f "fuchsia-$frandom" ];
+then
+  echo "Logo not found"
+  exit 1
+else
+  echo "Logo fuchsia-$((frandom)).png found"
+  exit 0
+fi
